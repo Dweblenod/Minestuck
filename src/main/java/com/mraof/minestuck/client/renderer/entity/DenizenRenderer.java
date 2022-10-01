32d8efd5f5e4189353bd80b797bc234bc6332378
Dweblenod <ryanpgunnison@gmail.com>
2022-10-01 19:14:18 -0400

    Isolating the changes in render size to just the denizen model, still trying to figure out methods of getting the glow to consistently show up in front of their face(no alt methods have improved it so far)

--- src/main/java/com/mraof/minestuck/client/renderer/entity/DenizenRenderer.java
+++ src/main/java/com/mraof/minestuck/client/renderer/entity/DenizenRenderer.java
@@ -4,10 +4,7 @@ import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
-import com.mojang.math.Matrix3f;
-import com.mojang.math.Matrix4f;
-import com.mojang.math.Quaternion;
-import com.mojang.math.Vector3f;
+import com.mojang.math.*;
 import com.mraof.minestuck.client.model.DenizenModel;
 import com.mraof.minestuck.entity.DenizenEntity;
 import net.minecraft.client.renderer.MultiBufferSource;
@@ -38,24 +35,31 @@ public class DenizenRenderer extends GeoEntityRenderer<DenizenEntity>
 	@Override
 	public void render(DenizenEntity denizen, float entityYaw, float partialTicks, PoseStack poseStack, MultiBufferSource renderTypeBuffer, int packedLightIn)
 	{
+		//scales up the denizen model
+		poseStack.pushPose();
 		poseStack.scale(2F, 2F, 2F);
 		super.render(denizen, entityYaw, partialTicks, poseStack, renderTypeBuffer, packedLightIn);
+		poseStack.popPose();
 		
 		RenderSystem.enableBlend();
-		RenderSystem.blendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
+		RenderSystem.blendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA); //important for transparency
 		
 		//float alphaSwing = (float) Math.min(Math.max((Math.sin(denizen.level.getGameTime() / 30F) + 240), 240), 255); //causes the denizen glow to pulse in opacity //TODO turns black and flickers
-		float scaleSwing = (float) (Math.sin(denizen.level.getGameTime() / 50F) + 5); //causes the denizen glow to change in size
+		float scaleSwing = (float) (Math.sin(denizen.level.getGameTime() / 50F) + 10); //causes the denizen glow to change in size
 		
+		//TODO with the current version of geckolib, getWorldPosition() is bugged and is not appropriate to use. Once its fixed it's use should be prioritized. https://github.com/bernie-g/geckolib/issues/293
+		//modelProvider.getModel(new ResourceLocation("minestuck", "geo/denizen.geo.json")).getBone("head").get().getWorldPosition();
 		Quaternion cameraQuaternion = this.entityRenderDispatcher.cameraOrientation();
 		Vec3 cameraVec3 = this.entityRenderDispatcher.camera.getPosition();
-		Vec3 denizenHeadVec = denizen.getEyePosition().add(denizen.getLookAngle().scale(5));
+		Vec3 denizenHeadVec = denizen.getEyePosition().add(denizen.getLookAngle().scale(10));
 		Vec3 angledToCamera = cameraVec3.subtract(denizenHeadVec); //creates a point between the camera and the eyes of the denizen that the glow will be positioned at in order to obfuscate the models face
+		//Vec3 angledToCamera = denizenHeadVec.subtract(cameraVec3);
 		double distanceBetweenEntities = cameraVec3.distanceTo(denizenHeadVec);
 		float eyeOffset = (float) (denizen.getEyeY() - denizen.getY());
 		poseStack.pushPose();
 		//poseStack.translate(denizen.getLookAngle().x * 7, eyeOffset - 4, denizen.getLookAngle().z * 7); //TODO figure out how to make the translation forward always face the camera so as to be projected in a radius in front of the denizens eyes
-		poseStack.translate((angledToCamera.x() * 3) / distanceBetweenEntities, angledToCamera.y() / distanceBetweenEntities + eyeOffset, (angledToCamera.z() * 3) / distanceBetweenEntities);
+		poseStack.translate((angledToCamera.x() * 8) / distanceBetweenEntities, angledToCamera.y() / distanceBetweenEntities + eyeOffset, (angledToCamera.z() * 8) / distanceBetweenEntities);
+		//poseStack.translate(angledToCamera.x(), angledToCamera.y(), angledToCamera.z());
 		poseStack.scale(scaleSwing, scaleSwing, scaleSwing);
 		poseStack.mulPose(cameraQuaternion);
 		poseStack.mulPose(Vector3f.YP.rotationDegrees(180.0F));
