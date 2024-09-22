f2a47a3948e111f85cf1a411cb6777116c93fc2e
dweb <ryanpgunnison@gmail.com>
2024-09-22 14:26:16 -0400

    - Added interactivity with other TorrentSession within TorrentScreen - Improved widget updating in TorrentScreen - Added helper functions

--- src/main/java/com/mraof/minestuck/network/MSPayloads.java
+++ src/main/java/com/mraof/minestuck/network/MSPayloads.java
@@ -56,6 +56,7 @@ public class MSPayloads
 				.play(GutterUpdatePacket.ID, GutterUpdatePacket::read, MSPacket.PlayToClient::handler)
 				.play(TorrentPackets.UpdateClient.ID, TorrentPackets.UpdateClient::read, MSPacket.PlayToClient::handler)
 				.play(TorrentPackets.ModifySeeding.ID, TorrentPackets.ModifySeeding::read, MSPacket.PlayToServer::handler)
+				.play(TorrentPackets.ModifyLeeching.ID, TorrentPackets.ModifyLeeching::read, MSPacket.PlayToServer::handler)
 				
 				.play(MiscContainerPacket.ID, MiscContainerPacket::read, MSPacket.PlayToServer::handler)
 				
